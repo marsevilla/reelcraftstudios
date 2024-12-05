fbc4d1aa60230bfb9b6f9094dd284673985e9fc9
Maria del Mar <sevillamariadelmar@gmail.com>
2024-12-05 16:36:54 +0100

    feat: list of the movies

---
+++ src/app/components/all/all.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie.model';
+import { MovieService } from '../../services/movie.service';
+
+
+@Component({
+  selector: 'app-all',
+  imports: [],
+  templateUrl: './all.component.html',
+  styleUrl: './all.component.css'
+})
+export class AllComponent implements OnInit {
+  movies: Movie[] = [];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.movieService.getMovies().subscribe(data => {
+      this.movies = data
+    });
+  }
+
+}
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie.model';
+import { MovieService } from '../../services/movie.service';
+import { CommonModule } from '@angular/common';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  movies: Movie[] = [];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.movieService.getMovies().subscribe(data => {
+      this.movies = data.movies.map(movie => ({
+        title: movie.title,
+        genre: movie.genre,
+        releaseDate: movie.release_date, // Map mismatched key
+        budget: movie.budget,
+        synopsis: movie.synopsis,
+        directors: movie.directors,
+        producers: movie.producers,
+        status: movie.status
+      }));
+    });
+  }
+}
