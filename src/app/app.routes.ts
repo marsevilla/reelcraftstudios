490c16290aa53f13342c0c5b09baaae98b38a47b
Maria del Mar Sevilla Escobar <sevillamariadelmar@gmail.com>
2024-12-05 10:55:25 +0100

    build: routes. Routes are ready to be used

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AllComponent } from './all/all.component';
+import { AddComponent } from './add/add.component';
+import { ModifyComponent } from './modify/modify.component';
+import { DetailComponent } from './detail/detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'all', component: AllComponent },
+  { path: 'add', component: AddComponent },
+  { path: 'modify/:id', component: ModifyComponent },
+  { path: 'detail/:id', component: DetailComponent },
+];
